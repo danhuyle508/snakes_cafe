ec2573e8265daff0cb42df6c9c1a22b3c43aa973
Dan Le <Danhuyle508@gmail.com>
2019-02-18 15:44:51 -0800

    completed

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -39,17 +39,18 @@ ask_to_order = """
 ***********************************
 """
 print(ask_to_order)
-item = ''
+item = input(ask_to_order)
 customer_order = {}
+customer_order[item] = 0
+customer_order[item] = customer_order[item] + 1
+message = f"{customer_order[item]} order of {item}  has been added to your order"
+print(message)
 
 while item != 'quit':
     item = input(ask_to_order)
-    
     if item == 'quit': break
     
     else:
-        print(item)
-        customer_order[item] = 0
-        number_of_orders = customer_order[item] + 1
-        message = f"{number_of_orders} order of {item}  has been added to your order"
+        customer_order[item] = customer_order[item] + 1
+        message = f"{customer_order[item]} order of {item}  has been added to your order"
         print(message)
